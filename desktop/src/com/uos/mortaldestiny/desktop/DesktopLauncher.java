312290948357cba4a2c90a79eb56f69140a9abd5
Jack5496 <mllerpeter68@yahoo.de>
2016-01-18 11:01:29 +0100

    libGDX Init

---
+++ desktop/src/com/uos/mortaldestiny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uos.mortaldestiny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uos.mortaldestiny.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
---
+++ ios/src/com/uos/mortaldestiny/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.uos.mortaldestiny;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.uos.mortaldestiny.GameClass;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new GameClass(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
