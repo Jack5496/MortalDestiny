8686537cedbae5872ee6220661d81ef48d7223f2
Jack5496 <mllerpeter68@yahoo.de>
2016-01-19 20:24:42 +0100

    Outsourced someMethods in Helpers

--- core/src/com/uos/mortaldestiny/Inputs/Helper.java
+++ core/src/com/uos/mortaldestiny/Inputs/Helper.java
@@ -1,15 +1,36 @@
 package com.uos.mortaldestiny.Inputs;
 
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
+import com.uos.mortaldestiny.GameClass;
+
 public class Helper{
-	
+		
 	public static float getDegree(double xpos, double ypos){
 		float angle = (float) Math.toDegrees(Math.atan2(ypos, xpos));
 		
 	    return -angle;
 	}
 	
-	public static float adjustMouseInput(float angleOfMouse){
-		return angleOfMouse-=90;
+	public static float getYawInDegree(Vector3 from, Vector3 to){
+		return getDegree(from.x-to.x, from.z-to.z);
+	}
+	
+	public static Vector3 getMousePointAt(float screenX, float screenY){
+		Vector3 tmpVector = new Vector3();
+		
+		Ray ray = GameClass.getInstance().cam.getPickRay(screenX, screenY);
+	    final float distance = -ray.origin.y / ray.direction.y;
+	    tmpVector.set(ray.direction).scl(distance).add(ray.origin);
+
+	    return tmpVector;
+	}
+	
+	public static Vector3 getVectorFromModelInstance(ModelInstance model){
+		Vector3 v = new Vector3();
+		model.transform.getTranslation(v);
+		return v;
 	}
 
 }
--- core/src/com/uos/mortaldestiny/Inputs/KeyboardHandler.java
+++ core/src/com/uos/mortaldestiny/Inputs/KeyboardHandler.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.input.GestureDetector.GestureListener;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -23,38 +24,25 @@ public class KeyboardHandler implements InputProcessor {
 	public KeyboardHandler(InputHandler inputHandler) {
 		this.inputHandler = inputHandler;
 	}
-	
-	public boolean downLeft(){
+
+	public boolean downLeft() {
 		return keys[Keys.LEFT] || keys[Keys.A];
 	}
-	
-	public boolean downRight(){
+
+	public boolean downRight() {
 		return keys[Keys.DOWN] || keys[Keys.S];
 	}
-	
-	public boolean upLeft(){
+
+	public boolean upLeft() {
 		return keys[Keys.UP] || keys[Keys.W];
 	}
-	
-	public boolean upRight(){
+
+	public boolean upRight() {
 		return keys[Keys.RIGHT] || keys[Keys.D];
 	}
-	
-	
+
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -86,28 +74,36 @@ public class KeyboardHandler implements InputProcessor {
 	public boolean keyTyped(char character) {
 		return false;
 	}
-
-	Vector3 tmpVector = new Vector3();
 	
+	public float getYawInDegreeOfModelWithMouse(int screenX, int screenY, ModelInstance model) {
+		Vector3 mv = Helper.getMousePointAt(screenX, screenY);
+		Vector3 miv = Helper.getVectorFromModelInstance(model);
+		return Helper.getYawInDegree(mv, miv);
+	}
+	
+	public void rotatePlayer(int screenX, int screenY){
+		ModelInstance model = GameClass.getInstance().playerInstance;
+		inputHandler.setDirection(getYawInDegreeOfModelWithMouse(screenX, screenY, model));
+	}
+
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
+		rotatePlayer(screenX, screenY);
 
-		Ray ray = GameClass.getInstance().cam.getPickRay(screenX, screenY);
-	    final float distance = -ray.origin.y / ray.direction.y;
-	    tmpVector.set(ray.direction).scl(distance).add(ray.origin);
-//	    GameClass.getInstance().playerInstance.transform.setTranslation(tmpVector);
-//		return true;
-//		
-//		double xpos = screenX - GameClass.getInstance().getWidth()/2.0;
-//		double ypos = screenY - GameClass.getInstance().getHeight()/2.0;
-//		
-//		
-	    Vector3 playerPosition = new Vector3();
-	    GameClass.getInstance().playerInstance.transform.getTranslation(playerPosition);
-		float dir = Helper.adjustMouseInput(Helper.getDegree(tmpVector.x-playerPosition.x, tmpVector.z-playerPosition.z));
-		inputHandler.setDirection(dir);
-//				
 		return true;
+	}
+	
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		rotatePlayer(screenX, screenY);
+		
+		return false;
 	}
 
 	@Override
